4bf194921cda79ffa4b89bafaa762526272468f1
Candra R Prasetya <candraramadhanp@gmail.com>
2020-11-27 09:49:08 +0800

    Update screens.dart

--- lib/screen/screens.dart
+++ lib/screen/screens.dart
@@ -1,15 +1,2 @@
 part of 'screens.dart';
 import 'package:flutter/material.dart'
-class LoginScreen extends StatefulWidget {
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      
-    );
-  }
-}
