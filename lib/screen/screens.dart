4402032cf4fe19a47f57aae730564777c883e64b
WahidFatahilah <wahidfatahilah404@gmail.com>
2020-11-27 08:22:02 +0700

    1

---
+++ lib/screen/screens.dart
@@ -0,0 +1,15 @@
+part of 'screens.dart';
+import 'package:flutter/material.dart'
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
